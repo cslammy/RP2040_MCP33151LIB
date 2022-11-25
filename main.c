2c5e47b09988d4748e8697eb1651def7e0bff376
cslammy <audiodiwhy@gmail.com>
2022-11-25 15:03:04 -0800

    Added test for UART, set cmake to use Serial 1

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <stdint.h>
 #include <stdio.h>
 #include "mcp33151.h"
+#include "spiRP2040.h"
+#include "UART.h"
 
 uint8_t main() {
 
@@ -11,8 +13,10 @@ uint8_t main() {
 stdio_init_all();
 
 //loop dukie here
-    while (3) {
-
+    while (1) {
+        //test stdout       
+        //for (uint8_t x = 0; x < 255; x++)
+        //printf("x is: %d \n",x);
      }
 return 0;
 }
--- uf2copy.sh
+++ uf2copy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cp '/home/clamm/Dropbox/raspberry pico/PICO-VSCODE-PROJECTS/AD9833DCO/build/MCP33151LIB.uf2'  /media/clamm/RPI-RP2/.
+cp '/home/clamm/Dropbox/raspberry pico/PICO-VSCODE-PROJECTS/MCP33151LIB/build/MCP33151LIB.uf2'  /media/clamm/RPI-RP2/.
 
